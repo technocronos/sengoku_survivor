18f04dd947e9d22fcbf52da058d86bd586121f0a
Alexander Zhukov <alexulter@gmail.com>
2025-10-31 12:18:47 +0300

    経験著ドロップの演出

---
+++ vee/Assets/ExpPiece.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using Vs;
+
+namespace SengokuSurvivors
+{
+    public class ExpPiece : MonoBehaviour
+    {
+        private bool isObtained = false;
+        private const float distance = 2.0f;
+
+        public void Obtain(GameObject target)
+        {
+            if (this.isObtained)
+            {
+                return;
+            }
+            this.isObtained = true;
+            this.StartCoroutine(this.Play(target));
+        }
+
+        private IEnumerator Play(GameObject target)
+        {
+            {
+                var dir = (target.transform.position - this.transform.position).normalized;
+                var pos1 = this.transform.position;
+                var pos2 = this.transform.position - dir * distance;
+                var elapsed = 0.0f;
+                while (elapsed <= 1.0f)
+                {
+                    this.transform.position = Vector3.Lerp(pos1, pos2, elapsed);
+                    elapsed += Time.deltaTime * 4;
+                    yield return null;
+                }
+            }
+            {
+                var pos1 = this.transform.position;
+                var elapsed = 0.0f;
+                while (elapsed <= 1.0f)
+                {
+                    this.transform.position = Vector3.Lerp(pos1, target.transform.position, elapsed);
+                    elapsed += Time.deltaTime * 4;
+                    yield return null;
+                }
+            }
+            SoundService.Instance.PlaySe("get_item");
+            this.OnComplete();
+            GameObject.Destroy(this.gameObject);
+            yield break;
+        }
+
+        private void OnComplete()
+        {
+            //add exp to player
+        }
+    }
+}
